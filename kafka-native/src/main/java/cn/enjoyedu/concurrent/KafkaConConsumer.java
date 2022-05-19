3e11afe5ca9d94145019cbc0d9e066a6b0875dce
Justin0704 <djlijian@126.com>
2022-05-19 22:39:30 +0800

    kafka - 多线程下使用生产者

---
+++ kafka-native/src/main/java/cn/enjoyedu/concurrent/KafkaConConsumer.java
@@ -0,0 +1,12 @@
+package cn.enjoyedu.concurrent;
+
+/**
+ * 多线程下的消费者
+ */
+public class KafkaConConsumer {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ kafka-native/src/main/java/cn/enjoyedu/concurrent/KafkaConProducer.java
@@ -0,0 +1,11 @@
+package cn.enjoyedu.concurrent;
+
+/**
+ * 多线程下的生产者
+ */
+public class KafkaConProducer {
+    public static void main(String[] args) {
+
+
+    }
+}
