34ec76e5dfe4754c7ab6691693e08286e04735fc
Justin0704 <djlijian@126.com>
2022-05-20 11:58:40 +0800

    kafka - 自定义序列化器

--- kafka-native/src/main/java/cn/enjoyedu/selfserial/SelfSerialConsumer.java
+++ kafka-native/src/main/java/cn/enjoyedu/selfserial/SelfSerialConsumer.java
@@ -1,8 +1,36 @@
 package cn.enjoyedu.selfserial;
 
+import cn.enjoyedu.constant.KafaConstant;
+import cn.enjoyedu.vo.UserVO;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+
 public class SelfSerialConsumer {
 
-    public static void main(String[] args) {
+    private static KafkaConsumer<String, UserVO> consumer = null;
 
+    public static void main(String[] args) {
+        consumer = new KafkaConsumer<String, UserVO>(KafaConstant.consumerConfig("selfserial",
+                StringDeserializer.class,
+                SelfDeserializer.class));
+        try{
+            consumer.subscribe(Collections.singletonList(KafaConstant.SELF_SERIAL_TOPIC));
+            while (true){
+                ConsumerRecords<String, UserVO> records = consumer.poll(Duration.ofMillis(500));
+                for(ConsumerRecord<String, UserVO> record : records){
+                    System.out.println(String.format(
+                            "主题：%s，分区：%d，偏移量：%d，key：%s，value：%s",
+                            record.topic(),record.partition(),record.offset(),
+                            record.key(),record.value()));
+                }
+            }
+        }finally {
+            consumer.close();
+        }
     }
 }
--- kafka-native/src/main/java/cn/enjoyedu/selfserial/SelfSerialProducer.java
+++ kafka-native/src/main/java/cn/enjoyedu/selfserial/SelfSerialProducer.java
@@ -1,4 +1,28 @@
 package cn.enjoyedu.selfserial;
 
+import cn.enjoyedu.constant.KafaConstant;
+import cn.enjoyedu.vo.UserVO;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
 public class SelfSerialProducer {
+    private static KafkaProducer<String, UserVO> producer = null;
+
+    public static void main(String[] args) {
+        /*消息生产者*/
+        producer = new KafkaProducer<String, UserVO>(KafaConstant.producerConfig(StringSerializer.class,SelfSerializer.class));
+        try{
+            ProducerRecord<String, UserVO> record;
+            try{
+                record = new ProducerRecord<String, UserVO>(KafaConstant.SELF_SERIAL_TOPIC, "user01", new UserVO(1, "justin"));
+                producer.send(record);
+                System.out.println("sent message ...");
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }finally {
+            producer.close();
+        }
+    }
 }
