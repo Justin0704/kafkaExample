1be680b02fa6917e318bd8dbc573c9357a165b4e
Justin0704 <djlijian@126.com>
2022-05-24 00:39:32 +0800

    kafka - spring整合

--- kafka-spring/src/main/java/cn/enjoyedu/service/KafkaConsumer.java
+++ kafka-spring/src/main/java/cn/enjoyedu/service/KafkaConsumer.java
@@ -1,4 +1,16 @@
 package cn.enjoyedu.service;
 
-public class KafkaConsumer {
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.listener.MessageListener;
+
+public class KafkaConsumer implements MessageListener<String, String>{
+
+    @Override
+    public void onMessage(ConsumerRecord<String, String> data) {
+        String name = Thread.currentThread().getName();
+        System.out.println(name+"|"+String.format(
+                "主题：%s，分区：%d，偏移量：%d，key：%s，value：%s",
+                data.topic(),data.partition(),data.offset(),
+                data.key(),data.value()));
+    }
 }
--- kafka-spring/src/main/java/cn/enjoyedu/service/SendListener.java
+++ kafka-spring/src/main/java/cn/enjoyedu/service/SendListener.java
@@ -1,4 +1,25 @@
 package cn.enjoyedu.service;
 
-public class SendListener {
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.kafka.support.ProducerListener;
+
+/**
+ * 类说明：发送者确认
+ */
+public class SendListener implements ProducerListener{
+    @Override
+    public void onSuccess(String topic, Integer partition, Object key, Object value, RecordMetadata recordMetadata) {
+        System.out.println("offset:"+recordMetadata.offset()+"-"
+                +"partition:"+recordMetadata.partition());
+    }
+
+    @Override
+    public void onError(String topic, Integer partition, Object key, Object value, Exception exception) {
+
+    }
+
+    @Override
+    public boolean isInterestedInSuccess() {
+        return true;
+    }
 }
