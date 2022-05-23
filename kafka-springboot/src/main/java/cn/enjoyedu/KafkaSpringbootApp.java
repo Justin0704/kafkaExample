d86ec8eadb41b1338162521432a45125ae3c86f1
Justin0704 <djlijian@126.com>
2022-05-24 01:16:52 +0800

    kafka - springboot整合

---
+++ kafka-springboot/src/main/java/cn/enjoyedu/KafkaSpringbootApp.java
@@ -0,0 +1,11 @@
+package cn.enjoyedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringbootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringbootApp.class, args);
+    }
+}
